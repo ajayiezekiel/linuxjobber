400cf0d2ebe6f6ff0335b3abebe2670b9b61f4f7
ezekiel <ajayiezekiel9@gmail.com>
2020-05-15 02:17:50 +0100

    created my three models for the ap p

--- ajayiezekiel9000scrumy/admin.py
+++ ajayiezekiel9000scrumy/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import ScrumyGoals, ScrumyHistory, GoalStatus
+
+admin.site.register(ScrumyGoals)
+admin.site.register(ScrumyHistory)
+admin.site.register(GoalStatus)
--- ajayiezekiel9000scrumy/models.py
+++ ajayiezekiel9000scrumy/models.py
@@ -1,3 +1,45 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.utils import timezone
+        
 
-# Create your models here.
+class GoalStatus(models.Model):
+    STATUS_CHOICES = (
+        ('pending', 'Pending'),
+        ('reviewing', 'Reviewing'),
+        ('approval', 'Approval')
+    )
+    status_name = models.CharField(
+                                max_length = 10,
+                                choices = STATUS_CHOICES,
+                                default='pending'
+                            )
+
+    def __str__(self):
+        return self.status_name
+
+class ScrumyGoals(models.Model):
+    goal_name =  models.CharField(max_length = 250)
+    goal_id = models.IntegerField(default = 1)
+    created_by = models.CharField(max_length = 250)
+    moved_by =  models.CharField(max_length = 250)
+    owner =  models.CharField(max_length = 250)
+    goal_status = models.ForeignKey(GoalStatus, on_delete = models.PROTECT)
+    user = models.ForeignKey(User, related_name='player', on_delete = models.PROTECT)
+
+
+    def __str__(self):
+        return self.goal_name
+
+
+class ScrumyHistory(models.Model):
+    moved_by =  models.CharField(max_length = 250)
+    created_by =  models.CharField(max_length = 250)
+    moved_from =  models.CharField(max_length = 250)
+    moved_to =  models.CharField(max_length = 250)
+    time_of_action = models.DateTimeField(default = timezone.now)
+    goal = models.ForeignKey(ScrumyGoals, on_delete = models.CASCADE)
+
+    def __str__(self):
+        return self.created_by
+
