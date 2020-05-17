1fee111931559036a6daa7a70da9b9e26780a878
ezekiel <ajayiezekiel9@gmail.com>
2020-05-17 03:04:57 +0100

    updated views and urls of the app

--- ajayiezekiel9000scrumy/urls.py
+++ ajayiezekiel9000scrumy/urls.py
@@ -3,5 +3,7 @@ from ajayiezekiel9000scrumy import views
 
 urlpatterns= [
     path('', views.get_grading_parameters, name="get_grading_parameters"),
-    path('movegoal/<int:goal_id>', views.move_goal, name="moved_goal" ),
+    path('movegoal/<int:goal_id>', views.move_goal, name="movegoal" ),
+    path('addgoal/', views.add_goal, name="addgoal"),
+    path('home/', views.home, name="home"),
 ]
--- ajayiezekiel9000scrumy/views.py
+++ ajayiezekiel9000scrumy/views.py
@@ -1,6 +1,9 @@
 from django.http import HttpResponse
+from django.contrib.auth.models import User
 
-from .models import ScrumyGoals
+from .models import ScrumyGoals, ScrumyHistory, GoalStatus
+
+import random
 
 def get_grading_parameters(request):
     goal = ScrumyGoals.objects.filter(goal_name='Learn Django')
@@ -9,3 +12,24 @@ def get_grading_parameters(request):
 def move_goal(request, goal_id):
     goal = ScrumyGoals.objects.get(goal_id=goal_id)
     return HttpResponse(goal)
+
+def add_goal(request):
+    weekly = GoalStatus.objects.get(status_name='Weekly Goal')
+    the_user = User.objects.get(username='louis')
+    sample_dict = {}
+    number = random.randint(1000,9999)
+    if number not in sample_dict:
+        ScrumyGoals.objects.create(goal_name='Keep Learning Django',
+                                    goal_id=number,
+                                    created_by='Louis',
+                                    moved_by='Louis',
+                                    owner='Louis',
+                                    goal_status=weekly,
+                                    user=the_user
+                                )
+        sample_dict[number] = number
+
+def home(request):
+    goals = ScrumyGoals.objects.filter(goal_name='Keep Learning Django')
+    output = ', '.join([eachgoal.goal_name for eachgoal in goals])
+    return HttpResponse(output)
