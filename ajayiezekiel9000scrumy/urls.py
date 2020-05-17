7ef5275ecd80e2cefe6df2e69d538f6b892d619a
ezekiel <ajayiezekiel9@gmail.com>
2020-05-17 01:15:36 +0100

    created a new view and modified the url file

--- ajayiezekiel9000scrumy/urls.py
+++ ajayiezekiel9000scrumy/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from ajayiezekiel9000scrumy import views
 
 urlpatterns= [
-    path('', views.get_grading_parameters, name="get_grading_parameters")
+    path('', views.get_grading_parameters, name="get_grading_parameters"),
+    path('movegoal/<int:goal_id>', views.move_goal, name="moved_goal" ),
 ]
--- ajayiezekiel9000scrumy/views.py
+++ ajayiezekiel9000scrumy/views.py
@@ -3,5 +3,9 @@ from django.http import HttpResponse
 from .models import ScrumyGoals
 
 def get_grading_parameters(request):
-    goal = ScrumyGoals.objects.get(pk=1)
+    goal = ScrumyGoals.objects.filter(goal_name='Learn Django')
+    return HttpResponse(goal)
+
+def move_goal(request, goal_id):
+    goal = ScrumyGoals.objects.get(goal_id=goal_id)
     return HttpResponse(goal)
